09c7948dd31ffa3871709d2cf4525b802b4662ac
Ярослав Бондарец <yar.bondarets@gmail.com>
2022-01-27 16:37:32 +0200

    Spaces fix in edit.jsx

--- src/edit.jsx
+++ src/edit.jsx
@@ -10,61 +10,61 @@ import ForgeUI, {
 import api, { route } from "@forge/api";
 
 const config = {
-	headers: {
-		'Accept': 'application/json'
-	}
+    headers: {
+        'Accept': 'application/json'
+    }
 }
 
 export const Edit = () => {
-	const onSubmit = (formValue) => {
-		return JSON.stringify(formValue)
-	}
+    const onSubmit = (formValue) => {
+        return JSON.stringify(formValue)
+    }
 
-	const {
-		extensionContext: { fieldValue },
-	} = useProductContext();
+    const {
+        extensionContext: { fieldValue },
+    } = useProductContext();
 
-	const selectedValues = JSON.parse(fieldValue)?.versions
+    const selectedValues = JSON.parse(fieldValue)?.versions
 
-	const [versions, setVersions] = useState([])
-	const [projects, setProjects] = useState([])
+    const [versions, setVersions] = useState([])
+    const [projects, setProjects] = useState([])
 
-	useEffect(async () => {
-		const res = await api.asApp().requestJira(route`/rest/api/3/project/search?typeKey=software`, config);
-		const data = await res.json();
+    useEffect(async () => {
+        const res = await api.asApp().requestJira(route`/rest/api/3/project/search?typeKey=software`, config);
+        const data = await res.json();
 
-		setProjects(data.values)
+        setProjects(data.values)
 
-		const promises = data.values.map(item => api.asApp().requestJira(route`/rest/api/3/project/${item.id}/version?maxResults=1000`, config))
-	
-		await Promise.all(promises)
+        const promises = data.values.map(item => api.asApp().requestJira(route`/rest/api/3/project/${item.id}/version?maxResults=1000`, config))
+    
+        await Promise.all(promises)
         .then(results => Promise.all(results.map(r => r.json())) )
         .then(resp => resp.map(project => project.values).flat())
         .then(resp => setVersions(resp))
-	}, [])
+    }, [])
 
-	return (
-			<CustomFieldEdit onSubmit={onSubmit} header="Cross Project Version Picker" width="medium" >
-					<Fragment>
-							<Select label="Versions" name="versions" isRequired isMulti>
-									{
-										versions.map(version => {
-											const label = projects.find(item => item.id == version.projectId)?.key + ' - ' + version.name + ' ' + (version.archived ? '(Archived)': version.released ? '(Released)' : '(Unrealesed)')
-											return (
-												<Option
-													defaultSelected={selectedValues && selectedValues.find(item => item.id === version.id)}
-													label={label}
-													value={{
-														label: label, 
-														id: version.id,
-														projectKey: projects.find(item => item.id == version.projectId)?.key 
-													}}
-												/>
-											)
-										})
-									}
-							</Select>
-					</Fragment>
-			</CustomFieldEdit>
-	);
+    return (
+            <CustomFieldEdit onSubmit={onSubmit} header="Cross Project Version Picker" width="medium" >
+                    <Fragment>
+                            <Select label="Versions" name="versions" isRequired isMulti>
+                                    {
+                                        versions.map(version => {
+                                            const label = projects.find(item => item.id == version.projectId)?.key + ' - ' + version.name + ' ' + (version.archived ? '(Archived)': version.released ? '(Released)' : '(Unrealesed)')
+                                            return (
+                                                <Option
+                                                    defaultSelected={selectedValues && selectedValues.find(item => item.id === version.id)}
+                                                    label={label}
+                                                    value={{
+                                                        label: label, 
+                                                        id: version.id,
+                                                        projectKey: projects.find(item => item.id == version.projectId)?.key 
+                                                    }}
+                                                />
+                                            )
+                                        })
+                                    }
+                            </Select>
+                    </Fragment>
+            </CustomFieldEdit>
+    );
 };
