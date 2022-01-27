0e049618c17ee6bf7634249388ac6b5e62581547
Ярослав Бондарец <yar.bondarets@gmail.com>
2022-01-27 16:39:13 +0200

    Spaces fix in view.jsx

--- src/view.jsx
+++ src/view.jsx
@@ -1,35 +1,35 @@
 import ForgeUI, {useProductContext, CustomField, Text, Link} from "@forge/ui";
 
 export const View = () => {
-	const {
-			extensionContext: { fieldValue },
-	} = useProductContext();
+    const {
+        extensionContext: { fieldValue },
+    } = useProductContext();
 
-	if (fieldValue == null) {
-		return (
-			<CustomField>
-				<Text>
-					None
-				</Text>
-			</CustomField>
-		)
-	}
+    if (fieldValue == null) {
+        return (
+            <CustomField>
+                <Text>
+                    None
+                </Text>
+            </CustomField>
+        )
+    }
 
-	const {versions} = JSON.parse(fieldValue)
+    const {versions} = JSON.parse(fieldValue)
 
-	if (versions && versions.length) {
-		return (
-			<CustomField>
-					{versions.map(version => <Text><Link href={`/projects/${version.projectKey}/versions/${version.id}/tab/release-report-all-issues`} target="_blank">{version.label}</Link></Text>)}
-			</CustomField>
-		);
-	}
+    if (versions && versions.length) {
+        return (
+            <CustomField>
+                    {versions.map(version => <Text><Link href={`/projects/${version.projectKey}/versions/${version.id}/tab/release-report-all-issues`} target="_blank">{version.label}</Link></Text>)}
+            </CustomField>
+        );
+    }
 
-	return (
-		<CustomField>
-			<Text>
-				None
-			</Text>
-		</CustomField>
-	)
+    return (
+        <CustomField>
+            <Text>
+                None
+            </Text>
+        </CustomField>
+    )
 };
