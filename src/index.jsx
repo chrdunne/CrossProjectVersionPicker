8ee7885e790d06bf2844b3380c09cde11ba07def
Ярослав Бондарец <yar.bondarets@gmail.com>
2022-01-31 16:39:03 +0200

    Validator deleted

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import ForgeUI, {render} from "@forge/ui";
 
 import {View} from "./view";
 import {Edit} from "./edit";
-import {ContextConfig} from "./context-config";
 
 export const runView = render(<View/>);
 export const runEdit = render(<Edit/>);
-export const runContextConfig = render(<ContextConfig/>);
